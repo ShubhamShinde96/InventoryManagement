910c2d528a359df6ef4f2d6708b3a0a97a0d4ef3
ShubhamShinde96 <shindesshubham96@gmail.com>
2024-11-03 20:04:26 +0530

    Pushing to github

---
+++ app/src/main/java/com/im/dairyinventorymanagement/HostActivity.kt
@@ -0,0 +1,19 @@
+package com.im.dairyinventorymanagement
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.im.dairyinventorymanagement.databinding.ActivityHostBinding
+
+class HostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
